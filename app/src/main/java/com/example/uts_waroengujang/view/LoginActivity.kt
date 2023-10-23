2ded019a1f82bcb6e8ab46067a34fc5b5166eb32
michaeldsnto <michaeldesanto80@gmail.com>
2023-10-24 01:30:34 +0700

    update fitur login

---
+++ app/src/main/java/com/example/uts_waroengujang/view/LoginActivity.kt
@@ -0,0 +1,50 @@
+package com.example.uts_waroengujang.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
+import com.example.uts_waroengujang.R
+import com.example.uts_waroengujang.viewmodel.LoginViewModel
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var viewModel:LoginViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        supportActionBar?.hide()
+
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+        viewModel.login()
+
+        btnSignIn.setOnClickListener {
+            val username = txtUsername.text.toString()
+            val password = txtPassword.text.toString()
+
+            val listUser = viewModel.usersLD.value
+
+            if (listUser != null) {
+                var isUserFound = false
+                for (user in listUser) {
+                    if (user.username == username && user.password == password) {
+                        isUserFound = true
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                }
+                if (!isUserFound) {
+                    Toast.makeText(this, "Username atau password salah",  Toast.LENGTH_SHORT).show()
+                }
+            }
+            else {
+                Toast.makeText(this, "Eror",  Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}
--- app/src/main/java/com/example/uts_waroengujang/view/MainActivity.kt
+++ app/src/main/java/com/example/uts_waroengujang/view/MainActivity.kt
@@ -2,13 +2,33 @@ package com.example.uts_waroengujang.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.example.uts_waroengujang.R
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navController = (supportFragmentManager.findFragmentById
+            (R.id.navHostFragment) as NavHostFragment).navController
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        val navView = findViewById<NavigationView>(R.id.navView)
+        NavigationUI.setupWithNavController(navView, navController)
     }
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, drawerLayout)
+                || super.onSupportNavigateUp()
+    }
+
 }
